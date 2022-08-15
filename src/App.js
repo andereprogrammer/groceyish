176d51e7da23eea4c14473515fb7bbf568cf67a2
Keshav <keshava.raj@decentro.tech>
2022-08-15 16:30:21 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router,Redirect,Route,Switch, useNavigate }from 'react-router-dom'
+import Home from './pages/Home';
+import Products from './pages/Products';
 
 function App() {
+  const history=0
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-main">
+      <div className="app-content">
+        <div className="snackbar"></div>
+        <div className="error-box"></div>
+        <Router history={history}>
+          <Switch>
+            <Route exact path='/'>
+              <Redirect to="/home"/>
+            </Route>
+            <Route path="/home" component={Home} />
+            <Route path="/product" component={Products}/>
+          </Switch>
+          
+        </Router>
+
+      </div>
+      
     </div>
   );
 }
---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Home.scss'
+
+function Home() {
+  return (
+    <div>
+        <h1>
+            HOME
+        </h1>
+    </div>
+  )
+}
+
+export default Home
